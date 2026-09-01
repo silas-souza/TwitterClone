a3777fa5dfda7bd34ee858d28b777ebebfcc35e3
silas souza <silas.souzameister1@gmail.com>
2026-09-01 13:26:05 -0300

    fix: use cloudinary for profile pictures

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 
 class User(AbstractUser):
     profile_picture = models.ImageField(
+        storage=MediaCloudinaryStorage(),
         upload_to="profile_pictures/",
         blank=True,
         null=True,
